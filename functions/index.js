c55785364920dbd85cd2141f582ccc52e0a5e471
malexj <alexmaximovj@gmail.com>
2025-01-23 13:47:21 +0200

    Add bot to function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,51 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const {Telegraf} = require("telegraf");
+const {
+  log,
+  debug,
+  error,
+} = require("firebase-functions/logger");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  telegram: {webhookReply: true},
+});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// error handling
+bot.catch((err, ctx) => {
+  error("[Bot] Error", err);
+  return ctx.reply(`Oops, encountered an error for ${ctx.updateType}`, err);
+});
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// initialize the commands
+bot.start((ctx) => ctx.reply("Hello! Send any message and I will copy it."));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+
+// copy every message and send to the user
+bot.on("message", (ctx) => {
+  debug("Message", ctx.message);
+  return ctx.telegram.sendCopy(ctx.chat.id, ctx.message);
+});
+
+// handle all telegram updates with HTTPs trigger
+exports.echoBot = functions.https.onRequest(async (request, response) => {
+  log("Incoming message", request.body);
+  return bot.handleUpdate(request.body, response)
+      .then((rv) => {
+        // rv represents the return value of the bot.handleUpdate() method.
+        if (!rv) {
+          // If rv is undefined (likely not a request from Telegram)
+          // send a 200 response
+          if (!response.headersSent) {
+            response.sendStatus(200);
+          }
+        }
+        return rv;
+      })
+      .catch((error) => {
+        console.error("Error handling update:", error);
+        if (!response.headersSent) {
+          // Send a 500 response if an error occurs
+          response.sendStatus(500);
+        }
+      });
 });
--- reqdme.md
+++ reqdme.md
@@ -5,3 +5,12 @@
 * Telegraf.js v4.16.3
 * Firebase CLI v9.2.0
 * Firebase project with Blaze plan.
+
+
+### Project setup
+
+create .env file with properties:
+
+```
+BOT_TOKEN=____TOKEN____
+```
